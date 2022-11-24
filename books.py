5ef6326d4546a30918b9e1b3e1ccfe5d0f837ec0
abe-mart <19436449+abe-mart@users.noreply.github.com>
2022-11-24 12:41:10 -0700

    Hardcode the correct profile

--- books.py
+++ books.py
@@ -13,6 +13,7 @@ from matplotlib.figure import Figure
 from pandas import json_normalize
 from PIL import Image
 from streamlit_lottie import st_lottie
+import string
 
 st.set_page_config(page_title="Goodreads Analysis App", layout="wide")
 
@@ -34,57 +35,12 @@ _lock = RendererAgg.lock
 
 
 sns.set_style("darkgrid")
-row0_spacer1, row0_1, row0_spacer2, row0_2, row0_spacer3 = st.columns(
-    (0.1, 2, 0.2, 1, 0.1)
-)
+st.title("The Book of Adam")
 
-row0_1.title("Analyzing Your Goodreads Reading Habits")
-
-
-with row0_2:
-    st.write("")
-
-row0_2.subheader(
-    "A Streamlit web app by [Tyler Richards](http://www.tylerjrichards.com), get my new book on Streamlit [here!](https://www.amazon.com/Getting-Started-Streamlit-Data-Science/dp/180056550X)"
-)
-
-row1_spacer1, row1_1, row1_spacer2 = st.columns((0.1, 3.2, 0.1))
-
-with row1_1:
-    st.markdown(
-        "Hey there! Welcome to Tyler's Goodreads Analysis App. This app scrapes (and never keeps or stores!) the books you've read and analyzes data about your book list, including estimating the gender breakdown of the authors, and looking at the distribution of the age and length of book you read. After some nice graphs, it tries to recommend a curated book list to you from a famous public reader, like Barack Obama or Bill Gates. One last tip, if you're on a mobile device, switch over to landscape for viewing ease. Give it a go!"
-    )
-    st.markdown(
-        "**To begin, please enter the link to your [Goodreads profile](https://www.goodreads.com/) (or just use mine!).** 👇"
-    )
-
-row2_spacer1, row2_1, row2_spacer2 = st.columns((0.1, 3.2, 0.1))
-with row2_1:
-    default_username = st.selectbox(
-        "Select one of our sample Goodreads profiles",
-        (
-            "89659767-tyler-richards",
-            "7128368-amanda",
-            "17864196-adrien-treuille",
-            "133664988-jordan-pierre",
-        ),
-    )
-    st.markdown("**or**")
-    user_input = st.text_input(
-        "Input your own Goodreads Link (e.g. https://www.goodreads.com/user/show/89659767-tyler-richards)"
-    )
-    need_help = st.expander("Need help? 👉")
-    with need_help:
-        st.markdown(
-            "Having trouble finding your Goodreads profile? Head to the [Goodreads website](https://www.goodreads.com/) and click profile in the top right corner."
-        )
-
-    if not user_input:
-        user_input = f"https://www.goodreads.com/user/show/{default_username}"
+user_input = "https://www.goodreads.com/user/show/113122191-adam-martin"
 
 user_id = "".join(filter(lambda i: i.isdigit(), user_input))
 user_name = user_input.split(user_id, 1)[1].split("-", 1)[1].replace("-", " ")
-
 
 @st.cache
 def get_user_data(
@@ -120,7 +76,7 @@ with line1_1:
         )
         st.stop()
 
-    st.header("Analyzing the Reading History of: **{}**".format(user_name))
+    st.header("Analyzing the Reading History of: **{}**".format(string.capwords(user_name)))
 
 df = json_normalize(contents["GoodreadsResponse"]["reviews"]["review"])
 u_books = len(df["book.id.#text"].unique())
