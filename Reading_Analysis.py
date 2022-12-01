c5279216b3bb9fcdacd748a5cdf21561498fc333
abe-mart <19436449+abe-mart@users.noreply.github.com>
2022-12-01 08:29:06 -0700

    Update Reading_Analysis.py

--- Reading_Analysis.py
+++ Reading_Analysis.py
@@ -109,9 +109,6 @@ with st.spinner('Getting book data'):
     df = df_out.copy()
     st.session_state['data'] = df
     
-# Get total number of Sanderson books read
-sander_count = df[df['book.authors.author.name'].str.contains('Sanderson')]['book.authors.author.name'].count()
-st.metric('Sanderson Books Read',sander_count)
 
 u_books = len(df["book.id.#text"].unique())
 u_authors = len(df["book.authors.author.id"].unique())
@@ -350,7 +347,7 @@ with row6_2, _lock:
 
 st.write("")
 
-# Wordclouds
+# Wordcloud
 st.write('The most common words in your book descriptions')
 desc = df['book.description'].dropna()
 text = strip_tags(' '.join(desc))
@@ -363,18 +360,31 @@ st.pyplot(fig)
 
 st.subheader('SanderStats')
 
+# Get total number of Sanderson books read
+sander_count = df[df['book.authors.author.name'].str.contains('Sanderson')]['book.authors.author.name'].count()
+st.metric('Sanderson Books Read',sander_count)
 
-# Ideas - show most obscure books and authors read, and most popular
+# Ideas - show most obscure books and authors read, and most popular - Done
 # Show series with unread books, or favorite authors with unread books - Done
-# Word cloud on book descriptions
+# Word cloud on book descriptions - Done
+# Your favorite and least favorite sanderson book, most recently read sanderson book, first added sanderson book, longest, shortest
+
+st.subheader('Popularity')
+
+# Most obscure books
+df['book.ratings_count'] = pd.to_numeric(df['book.ratings_count'])
+df_rating_sort = df.sort_values('book.ratings_count',ascending=False)
+# df_rating_sort['book.title'].head(15)
+st.write('Your most popular books (based on rating counts)')
+st.table(df_rating_sort[['book.title','book.authors.author.name','book.ratings_count']].head(15))
 
 st.subheader('Obscurity')
 
 # Most obscure books
 df['book.ratings_count'] = pd.to_numeric(df['book.ratings_count'])
 df_rating_sort = df.sort_values('book.ratings_count')
-df_rating_sort['book.title'].head(10)
+# df_rating_sort['book.title'].head(15)
 st.write('Your most obscure books (based on rating counts)')
-st.table(df_rating_sort[['book.title','book.authors.author.name']].head(10))
+st.table(df_rating_sort[['book.title','book.authors.author.name','book.ratings_count']].head(15))
 
 
