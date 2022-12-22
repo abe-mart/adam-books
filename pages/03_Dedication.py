b14a9e493d57291a7e3f31f147ba3911434eb87d
abe-mart <19436449+abe-mart@users.noreply.github.com>
2022-12-22 11:57:10 -0700

    Add dedication page.

---
+++ pages/03_Dedication.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+st.write('Adam, the conqueror of the chemical engineering program,
+A master of strategy and tactics, unrivaled in Age of Empires.
+His mind a forge, crafting solutions to complex problems with ease.
+
+A true hero, he stood tall and proud,
+Defying all odds, never backing down.
+Through countless hours of hard work and dedication,
+He emerged victorious, a true champion of his profession.
+
+But Adam's greatness extends far beyond the laboratory and the computer screen.
+He is a kind and generous soul, always willing to lend a helping hand.
+His warm smile and infectious laugh bring joy to all those around him.
+
+So let us raise a glass to Adam, the hero we all admire.
+May his legend live on forever, inspiring us all to be the best we can be.')
